d5c279acf6c4fbded5467b79725a85fb93d273d1
NMShihab <nmshihabislam@gmail.com>
2021-05-06 00:06:16 +0600

    dynamic list

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -8,30 +8,19 @@ function Expenses(props) {
   const filterYearHandler = (selectedYear) => {
     setYear(selectedYear);
   };
-  console.log(year);
+
   return (
     <Card className="expenses">
       <ExpensesFilter selected={year} onChangeFilterYear={filterYearHandler} />
-      <ExpenseItem
-        expenseDate={props.expenses[0].date}
-        expenseItem={props.expenses[0].title}
-        expensePrice={props.expenses[0].amount}
-      />
-      <ExpenseItem
-        expenseDate={props.expenses[1].date}
-        expenseItem={props.expenses[1].title}
-        expensePrice={props.expenses[1].amount}
-      />
-      <ExpenseItem
-        expenseDate={props.expenses[2].date}
-        expenseItem={props.expenses[2].title}
-        expensePrice={props.expenses[2].amount}
-      />
-      <ExpenseItem
-        expenseDate={props.expenses[3].date}
-        expenseItem={props.expenses[3].title}
-        expensePrice={props.expenses[3].amount}
-      />
+      {props.expenses.map((expense) => {
+        return (
+          <ExpenseItem
+            expenseDate={expense.date}
+            expenseItem={expense.title}
+            expensePrice={expense.amount}
+          />
+        );
+      })}
     </Card>
   );
 }
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import NewExpenseForm from "./NewExpenseForm";
 
 const NewExpense = (props) => {
+  const [pkey, setPkey] = useState(0);
   const expenseSaveHandler = (expense) => {
+    setPkey(pkey + 1);
     const expenseData = {
+      id: pkey,
       ...expense,
     };
     console.log(expenseData);
